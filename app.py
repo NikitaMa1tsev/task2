62b6a2406764a6f79b11e8cbe3c8e00b7fa598ee
NikitaMa1tsev <87598626+NikitaMa1tsev@users.noreply.github.com>
2025-07-23 12:00:33 +0300

    Update app.py

--- app.py
+++ app.py
@@ -166,7 +166,7 @@ with tab3:
         .merge(goods)
         .groupby('GoodDesc', as_index=False)
         .agg(AvgRating=('Rating', 'mean'))
-    
+    )
     col1, col2 = st.columns(2)
     
     with col1:
